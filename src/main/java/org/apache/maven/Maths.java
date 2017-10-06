ccb4355c55f0ae2d534d2b50803bd697cf5888eb
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-10-06 13:24:57 +0200

    Added some code

---
+++ src/main/java/org/apache/maven/Maths.java
@@ -0,0 +1,8 @@
+package org.apache.maven;
+
+public class Maths {
+	public static int sum(int a, int b)
+	{
+		return a+b;
+	}
+}
---
+++ src/test/java/org/apache/maven/TestMaths.java
@@ -0,0 +1,19 @@
+package org.apache.maven;
+
+import junit.framework.TestCase;
+
+public class TestMaths extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testSum() {
+		assertEquals(Maths.sum(2, 3),5);
+	}
+
+}
