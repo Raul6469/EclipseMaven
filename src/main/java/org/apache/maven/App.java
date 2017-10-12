2b0950ff3eff76eaf9da4c3347f00315c4cd73b3
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-10-12 20:30:49 +0200

    Try push it on Travis

--- src/main/java/org/apache/maven/App.java
+++ src/main/java/org/apache/maven/App.java
@@ -1,13 +1,36 @@
 package org.apache.maven;
 
+import javafx.application.Application;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("ListView Experiment 1");
+
+        ListView listView = new ListView();
+
+        listView.getItems().add("Item 1");
+        listView.getItems().add("Item 2");
+        listView.getItems().add("Item 3");
+        
+
+        HBox hbox = new HBox(listView);
+        
+        Scene scene = new Scene(hbox, 200, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
 }
